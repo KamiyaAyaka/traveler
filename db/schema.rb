ac5318e334a9464ee9ec68a48f53f903b13c9ee4
KamiyaAyaka <126169966+KamiyaAyaka@users.noreply.github.com>
2026-07-19 00:10:21 +0900

    [add]ユーザ紹介・一覧ページ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_07_12_133040) do
+ActiveRecord::Schema[7.1].define(version: 2026_07_18_143342) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_07_12_133040) do
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -70,6 +71,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_07_12_133040) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
